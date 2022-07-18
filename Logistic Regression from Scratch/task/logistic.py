8f4274f97e14538369e892707dc6953fa58b2d2f
fvn70 <fvn_reg@mail.ru>
2022-07-18 12:40:36 +0700

    Stage 3/4: Log-Loss

--- Logistic Regression from Scratch/task/logistic.py
+++ Logistic Regression from Scratch/task/logistic.py
@@ -21,27 +21,40 @@ class CustomLogisticRegression:
         return self.sigmoid(t)
 
     def fit_mse(self, X_train, y_train):
-        k = 0
         self.coef_ = [0. for _ in range(X_train.shape[1])]
         if self.fit_intercept:
             self.coef_ = [0.] + self.coef_
             k = 1
+        else:
+            k = 0
 
         for l in range(self.n_epoch):
             for i, row in enumerate(X_train):
                 y_hat = self.predict_proba(row, self.coef_)
                 delta = self.l_rate * (y_hat - y_train[i]) * y_hat * (1 - y_hat)
                 self.coef_[0] = self.coef_[0] - delta
-                # print('delta = ', delta)
+                for j in range(len(row)):
+                    self.coef_[j + k] = self.coef_[j + k] - delta * row[j]
+
+    def fit_log_los(self, X_train, y_train):
+        N = X_train.shape[0]
+        self.coef_ = [0. for _ in range(X_train.shape[1])]
+        if self.fit_intercept:
+            self.coef_ = [0.] + self.coef_
+            k = 1
+        else:
+            k = 0
+
+        for l in range(self.n_epoch):
+            for i, row in enumerate(X_train):
+                y_hat = self.predict_proba(row, self.coef_)
+                delta = self.l_rate * (y_hat - y_train[i]) / N
+                self.coef_[0] = self.coef_[0] - delta
                 for j in range(len(row)):
                     self.coef_[j + k] = self.coef_[j + k] - delta * row[j]
 
     def predict(self, X_test, cut_off=0.5):
-        predictions = []
-        for row in X_test:
-            y_hat = self.predict_proba(row, self.coef_)
-            predictions.append(1 if y_hat >= cut_off else 0)
-        return np.array(predictions)
+        return [int(self.predict_proba(row, self.coef_) >= cut_off) for row in X_test]
 
 
 from sklearn.datasets import load_breast_cancer
@@ -56,7 +69,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 clr = CustomLogisticRegression(fit_intercept=True, l_rate=0.01, n_epoch=1000)
 
-clr.fit_mse(X_train, y_train.to_numpy())
+# df = pd.read_csv('example_stage2-3.txt')
+# X_train = df[['worst concave points', 'worst perimeter', 'worst radius']].to_numpy()
+# y_train = df['y'].to_numpy()
+
+# clr.fit_mse(X_train, y_train.to_numpy())
+clr.fit_log_los(X_train, y_train.to_numpy())
 y_pred = clr.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
 
